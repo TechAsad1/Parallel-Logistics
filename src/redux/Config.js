4d2d579fdb37106693e353f3cc9537fb04fcdae2
TechAsad1 <techinfo517@gmail.com>
2025-12-31 22:38:02 +0500

    Trigger rebuild on Vercel 2

--- src/redux/Config.js
+++ src/redux/Config.js
@@ -1,5 +1,7 @@
 const Config = {
-    url: "http://localhost:5259/api/",
-    urlImage: "http://localhost:5259/",
+    url: "https://membership.bsite.net/api/",
+    urlImage: "https://membership.bsite.net/",
+    // url: "http://localhost:5259/api/",
+    // urlImage: "http://localhost:5259/",
 };
 export default Config;
